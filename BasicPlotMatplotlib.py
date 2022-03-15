ea0af033d5d08310cadebaf30b4245f3058fb028
Han <101443278+Handra1@users.noreply.github.com>
2022-03-15 12:01:19 +0700

    Add files via upload

---
+++ BasicPlotMatplotlib.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.792]
+plt.plot(year, pop)
+plt.show()
+
+"""grafik dengan titik"""
+plt.scatter(year, pop)
+plt.show()
